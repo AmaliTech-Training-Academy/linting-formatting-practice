c0d06dd73ef264d5420400a40e83b38574b5f462
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-09-12 15:47:39 +0200

    add linter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
+// import { AppComponent } from './app.component';
+// import { UserListComponent } from './user-list/user-list.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [],
+//   imports: [BrowserModule],
+//   providers: [],
+//   bootstrap: [],
+// })
+// export class AppModule {}
