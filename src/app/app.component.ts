7e05cd5c2cf1c7ed58dc2938a9b3940fdb441c1d
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-09-13 08:04:30 +0200

    add image component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { UserListComponent } from "./user-list/user-list.component";
+import { ImageComponent } from "./image/image.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   standalone: true,
-  imports: [UserListComponent],
+  imports: [UserListComponent, ImageComponent],
 })
 export class AppComponent {}
